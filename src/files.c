f9cbaef3c5eb61691ae719d3a2a332c5ca21f93a
Stefan Milojevic <stefanmilojevic87@gmail.com>
2024-08-30 08:31:18 +0200

    WIP #2 (delete before merge)

--- src/files.c
+++ src/files.c
@@ -1,10 +1,15 @@
 #include "logger.h"
+#include <config.h>
 #include <dirent.h>
 #include <errno.h>
 #include <pwd.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+char *vc_rootpath;
 
 void set_homedir(const char **dir) {
   struct passwd *pw = getpwuid(getuid());
@@ -17,44 +22,67 @@ void set_homedir(const char **dir) {
 }
 
 bool has_cache(const char *dir) {
+  char *cache_dirpath;
+  asprintf(&cache_dirpath, "%s/%s", dir, ".cache");
+
+  DIR *cache_dir = opendir(cache_dirpath);
+  if (!cache_dir && errno != ENOENT) {
+    LOG_ERROR("Couldn't open %s: %s", cache_dirpath, strerror(errno));
+    free(cache_dirpath);
+    return false;
+  }
+
+  // cache_dir doesn't exist
+  if (!cache_dirpath && errno == ENOENT) {
+    free(cache_dirpath);
+    return false;
+  }
+
+  closedir(cache_dir);
+  free(cache_dirpath);
   return true;
 }
 
 void set_standard_root(char **dir) {
-  const char *homedirname = NULL;
-  set_homedir(&homedirname);
-  if (!homedirname) {
+  const char *home_dirpath = NULL;
+  set_homedir(&home_dirpath);
+  if (!home_dirpath) {
     *dir = NULL;
     return;
   }
 
-  /* if has_cache:
-   *  homedir + .cache + vibe-check
-   * else
-   *  homedir + .vibe-check
-   */
+  int  s;
+  if (has_cache(home_dirpath)) {
+    s = asprintf(dir, "%s/%s/%s", home_dirpath, ".cache", PACKAGE_NAME);
+  } else {
+    s = asprintf(dir, "%s/%s", home_dirpath, "." PACKAGE_NAME);
+  }
+  if (s <= 0) {
+    LOG_ERROR("Can't open %s: Not enough memory", *dir);
+  }
 }
 
-bool set_root(const char *custom_root_path) {
-  DIR *vc_root;
-  if (!custom_root_path) {
-
-    char *standard_root = NULL;
-    set_standard_root(&standard_root);
-    if (!standard_root) {
-      return false;
-    }
-
-    vc_root = opendir(standard_root);
-  } else {
-    vc_root = opendir(custom_root_path);
+bool set_rootpath(const char *custom_root_path) {
+  int s = asprintf(&vc_rootpath, "%s", custom_root_path);
+  if (s <= 0) {
+    LOG_ERROR("Can't open %s: Not enough memory", custom_root_path);
   }
 
+  if (!vc_rootpath) {
+    set_standard_root(&vc_rootpath);
+    if (!vc_rootpath) {
+      return false;
+    }
+  }
+
+  DIR *vc_root = opendir(vc_rootpath);
   if (!vc_root) {
     LOG_ERROR("Can't open %s: %s", custom_root_path, strerror(errno));
     return false;
   }
-
   closedir(vc_root);
+
   return true;
 }
+
+void free_rootpath() { free(vc_rootpath); }
